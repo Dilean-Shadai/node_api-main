6b477946cef373fcc4c620f722a0cee596e58b27
Dilean-Shadai <dilean.garcia.ing@gmail.com>
2024-12-03 19:04:22 -0600

    refactor: se quito unas lineas y comentarios no necesarios

--- src/services/admin.js
+++ src/services/admin.js
@@ -2,17 +2,12 @@
 const {Router} = require('express');
 const { add, view, obtenerUsuariosAPI } = require('../controllers/users');
 
-
 const router = Router();
 
-router.post('/api/add', add); // Agregar usuario sin validación de token
+router.post('/api/add', add); // 
 
-router.get('/api/list', view); // Obtener lista de usuarios sin validación de token
+router.get('/api/list', view); // 
 
-router.get('/api/usuarios', obtenerUsuariosAPI); // para formato json
-
-//router.post('/api/user/edit/:id'); // Editar usuario sin validación de token
-
-//router.post('/api/user/delete/:id'); // Eliminar usuario sin validación de token
+router.get('/api/usuarios', obtenerUsuariosAPI); // Esta Ruta si la usamos de momento
 
 module.exports = router;
